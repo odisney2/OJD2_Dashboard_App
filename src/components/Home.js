9124562c92fc2a5d9f026c7e1de26437006a0710
odisney2 <odisney2@gmail.com>
2018-11-07 15:26:21 -0500

    Got a rough draft of home page setup

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Home extends Component {    
+  
+  render() {
+    return (
+      <div className="main-content home">
+        <h2>OJD2 Dashboard App</h2>
+        <p>This app will have weather information along with Lego information.</p>
+        <hr />
+        
+      </div>
+    );
+  }
+}
+
+export default Home;
