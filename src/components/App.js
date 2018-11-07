a103adfbc158c0b0844fe2d976548e7e3003bdbd
odisney2 <odisney2@gmail.com>
2018-11-07 15:26:02 -0500

    Got a rough draft of home page setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
+//import React, { Component } from 'react';
+//import logo from '../logo.svg';
+import React from 'react';
+
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom';
+
+// App components
+import Home from './Home';
+
+// CSS files to import
 import '../css/App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+      <Route path="/" component={Home} />
+    </div>
+  </BrowserRouter>
+);
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
